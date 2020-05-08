f6ba1e71ac241df925ef9bdfc6f8f9352247b169
Christopher Rugg <christophersrugg#gmail.com>
2020-05-08 09:23:57 -0400

    files added

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# 05 Third-Party APIs: Work Day Scheduler
+# Homework5
+
 
 Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
--- script.js
+++ script.js
@@ -1,42 +1,58 @@
-/*
-1.GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar*/
 
-var fullDate = new Date()
-console.log(fullDate);
-//Thu May 19 2011 17:25:38 GMT+1000 {}
- 
-//convert month to 2 digits
-var twoDigitMonth = ((fullDate.getMonth().length+1) === 1)? (fullDate.getMonth()+1) : '0' + (fullDate.getMonth()+1);
- 
-var currentDate = fullDate.getDate() + "/" + twoDigitMonth + "/" + fullDate.getFullYear();
-console.log(currentDate);
-//19/05/2011
+//Date 
+var date = moment().format("MM Do YYYY");
+// var currentTime = moment().format('H');
+// console.log(currentTime);
+
+//append date
+$("#currentDay").append(date);
 
 
-/*2.WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-*/
+//Time of day
+
+var timeOfday = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
+updatetime();
+
+function updatetime() {
+  var currentTime = moment().format('H');
+  for(var i = 0; i < timeOfday.length; i++) {
+
+    if (parseInt(timeOfday[i]) > currentTime) {
+      $("#" + timeOfday[i]).attr("style", "background-color: #58ce7b");
+
+
+    }
+    else if (parseInt(timeOfday[i]) < currentTime) {
+      $("#" + timeOfday[i]).attr("style", "background-color: lightgray");
+
+    }
+    else if (parseInt(timeOfday[i]) == currentTime) {
+      $("#" + timeOfday[i]).attr("style", "background-color: #fc665e");
+    
+    }
+  }
+}
+
+//onclick event to save user input to local storage
+$(".saveBtn").on("click", function() {
+    var timeOfday = $(this).parent().attr("id");
+    var textContent = $("input").val().trim();
+
+    localStorage.setItem(timeOfday, textContent);
+    //console.log(timeOfDay, textContent);
+});
+
+
+$("#9am").children("input").val(localStorage.getItem("9am"));
+$("#10am").children("input").val(localStorage.getItem("10am"));
+$("#11am").children("input").val(localStorage.getItem("11am"));
+$("#12pm").children("input").val(localStorage.getItem("12pm"));
+$("#1pm").children("input").val(localStorage.getItem("1pm"));
+$("#2pm").children("input").val(localStorage.getItem("2pm"));
+$("#3pm").children("input").val(localStorage.getItem("3pm"));
+$("#4pm").children("input").val(localStorage.getItem("4pm"));
+$("#5pm").children("input").val(localStorage.getItem("5pm"));
 
 
 
-/*3.WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-*/
 
-
-/*4.WHEN I click into a timeblock
-THEN I can enter an event
-*/
-
-
-
-/*5. WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-*/
-
-
-/*6. WHEN I refresh the page
-THEN the saved events persist
-*/
